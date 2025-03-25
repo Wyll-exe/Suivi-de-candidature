1fb5270b3f9f417504f8e33e8089f839eaf3c47c
Zaco <cyrilmathe08@gmail.com>
2025-03-25 11:51:54 +0100

    :sparkles: ajout du model de la bdd

--- backend/model/model.js
+++ backend/model/model.js
@@ -1,1 +1,53 @@
-// model
+// model pour la bdd
+import Joi, { required } from "joi";
+import mongoose from "mongoose";
+
+const suiviSchema = new mongoose.Schema({
+    society : {
+        type: String,
+        required: true
+    },
+    job : {
+        type: String,
+        required: true
+    },
+    link : {
+        type: String,
+    },
+    send_date : {
+        type: Number,
+        required: true
+    },
+    status : {
+        type: String,
+        enum: ['En attente', 'Acceptée', 'Refusée'],
+        required: true
+    }
+})
+
+const suivi = mongoose.model('suivi', suiviSchema)
+
+const suiviValidation = Joi.object({
+    society: Joi.string()
+    .required()
+    .messages({
+        'string.empty': 'Le nom de société est obligatoire'
+    }),
+    job: Joi.string()
+    .required()
+    .messages({
+        'string.empty': 'Le poste est obligatoire'
+    }),
+    send_date: Joi.string()
+    .required()
+    .messages({
+        'string.empty': 'La date est obligatoire'
+    }),
+    status: Joi.string()
+    .required()
+    .messages({
+        'string.empty': 'Le status n\'est pas bien définis'
+    }),
+})
+
+export { suivi, suiviValidation }
