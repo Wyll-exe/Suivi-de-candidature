80cf77ea540624e53b3d22c6b129d87038361bf0
Zaco <cyrilmathe08@gmail.com>
2025-03-25 16:25:03 +0100

    :pencil2: typo

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,6 @@ app.use(express.json());
 
 app.use("/api/v1", routes)
 
-app.get("/test", (req, res) => {
-    res.send('TEST')
-})
-
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 });
