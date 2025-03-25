93c840d6b978fd8ab37486d663845e94b4e3a350
L0wBly <arthurboutry2005@icloud.com>
2025-03-25 16:58:53 +0100

    :lipstick: Ajout du style pour les page d'affichage et de création

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,7 +1,15 @@
+import CreationCandidature from "./components/CreationCandidature";
+import AffichageCandidature from "./components/AffichageCandidature"
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 function App() {
   return (
-  <div className="bg-red-500 w-screen h-[100vh]"></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AffichageCandidature />} />
+        <Route path='/CreationCandidature' element={<CreationCandidature />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/app/src/components/CreationCandidature.jsx
@@ -0,0 +1,31 @@
+import '../style.css';
+import { Link } from "react-router";
+
+function CreationCandidature() {  
+
+return (
+    <div className='flex h-[100vh] w-screen'>
+        <div className="bg-black w-sm h-[100vh] flex flex-col items-center fixed">
+            <h1 className='text-white text-[2.5rem] p-[2rem] whitespace-nowrap'>Mes Candidatures</h1>
+            <div className='flex flex-col gap-[4rem] pt-[4rem] w-[100%] h-auto'>
+                <div className='border-y-2 border-solid border-white w-[100%] h-[4rem] flex justify-center items-center'>
+                    <h2 className='text-white text-[1.5rem]'>Ajouter une Candidature</h2>
+                </div>
+                <div className='border-y-2 border-solid border-white w-[100%] h-[4rem] flex justify-center items-center'>
+                    <h2 className='text-white text-[1.5rem]'>Suivi des relances</h2>
+                </div>
+                <div className='border-y-2 border-solid border-white w-[100%] h-[4rem] flex justify-center items-center'>
+                    <h2 className='text-white text-[1.5rem]'>Statistiques</h2>
+                </div>
+            </div>
+            <Link className='text-white text-[1.5rem] bottom-[0] absolute' to="/">Retour au Home</Link>
+        </div>
+        <div className='pl-[24rem] w-screen'>
+            <h2>vdsd</h2>
+        </div>
+    </div>
+    
+)  
+}
+
+export default CreationCandidature
