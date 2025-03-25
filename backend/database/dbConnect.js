d813f1fb972103202e28764c614cc591a536692d
Zaco <cyrilmathe08@gmail.com>
2025-03-25 10:41:27 +0100

    :sparkles: ajout de la connexion a la bdd

--- backend/database/dbConnect.js
+++ backend/database/dbConnect.js
@@ -1,1 +1,14 @@
-// database
+// connecter la db
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/contentDB')
+    console.log(`MongoDB connecté`);
+  } catch (error) {
+    console.error(` Problème lors de la connection`, error);
+    process.exit(1)
+  }
+}
+
+export default connectDB
