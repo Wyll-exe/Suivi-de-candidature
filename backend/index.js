35dbe8ca82157d8588242bbe199e09f1e319522e
Zaco <cyrilmathe08@gmail.com>
2025-03-25 14:32:00 +0100

    :bug:

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import routes from "./routes/routes.js";
 import connectDB from "./database/dbConnect.js";
 
@@ -6,6 +7,8 @@ const app = express();
 const port = 3000;
 
 connectDB();
+
+app.use(cors())
 
 app.use(express.json());
 
