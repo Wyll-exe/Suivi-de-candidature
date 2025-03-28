663d75eadb9f16c549dbb3e811e120bc8bdb2e45
L0wBly <arthurboutry2005@icloud.com>
2025-03-28 16:09:14 +0100

    :art: Ajout style page principal

--- frontend/app/src/components/AffichageCandidature.jsx
+++ frontend/app/src/components/AffichageCandidature.jsx
@@ -60,36 +60,39 @@ function AffichageCandidature() {
                         </div>
                         <Link className='text-white text-[1.5rem] bottom-[0] absolute' to="/">Retour au Home</Link>
                     </div>
-                    <div className='pl-[26rem] pt-[2rem] pr-[2rem] w-screen overflow-x-hidden'>
-                        <div className='flex flex-col gap-[4rem]'>
+                    <div className='pl-[26rem] pt-[2rem] pr-[2rem] w-screen overflow-x-hidden bg-cyan-50 flex flex-col items-center'>
+                        <div className='flex flex-col gap-[4rem] w-[100%] max-w-[1300px]'>
                         {loading && <p>Loading...</p>}
                         {error && <p>{error.message}</p>}
                         {test?.map(post => (
                             <>
-                            <div className='flex justify-evenly pt-[2rem] pb-[2rem]'>
-                                <div className='flex flex-col gap-[1.5rem]'>
+                            <div className='flex justify-evenly h-[10rem] pt-[2rem] pb-[2rem] border-2 border-solid border-black bg-cyan-200 rounded-[20px] w-[100%] max-w-[1400px] drop-shadow-2xl'>
+                                <div className='flex flex-col justify-center items-center gap-[1.5rem] border-2 border-solid border-black rounded-[20px] px-[1.5rem] py-[2rem] bg-violet-300'>
                                     <p>Society</p>
                                     <p>{post.society}</p>
                                 </div>
-                                <div className='flex flex-col gap-[1.5rem]'>
-                                    <p>Society</p>
+                                <div className='flex flex-col justify-center items-center gap-[1.5rem] border-2 border-solid border-black rounded-[20px] px-[1.5rem] py-[2rem] bg-violet-300'>
+                                    <p>Job</p>
                                     <p>{post.job}</p>
                                 </div>
-                                <div className='flex flex-col gap-[1.5rem]'>
-                                    <p>Society</p>
+                                <div className='flex flex-col justify-center items-center gap-[1.5rem] border-2 border-solid border-black rounded-[20px] px-[1.5rem] py-[2rem] bg-violet-300'>
+                                    <p>Lien du post</p>
                                     <p>{post.job_link}</p>
                                 </div>
-                                <div className='flex flex-col gap-[1.5rem]'>
-                                    <p>Society</p>
+                                <div className='flex flex-col justify-center items-center gap-[1.5rem] border-2 border-solid border-black rounded-[20px] px-[1.5rem] py-[2rem] bg-violet-300'>
+                                    <p>Date</p>
                                     <p>{new Date(post.send_date).toDateString()}</p>
                                 </div>
-                                <div className='flex flex-col gap-[1.5rem]'>
-                                    <p>Society</p>
+                                <div className='flex flex-col justify-center items-center gap-[1.5rem] border-2 border-solid border-black rounded-[20px] px-[1.5rem] py-[2rem] bg-violet-300'>
+                                    <p>Status</p>
                                     <p>{post.status}</p>
                                 </div>
+                                <div className='flex flex-col justify-center gap-[1rem]'>
+                                    <Link to={`/${post._id}`} class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Modifier</Link>
+                                    <Link to={`/1/${post._id}`} class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Supprimer</Link>
+                                </div>
                             </div>
-                            <Link to={`/${post._id}`}>Modifier</Link>
-                            <Link to={`/1/${post._id}`} >Supprimer</Link>
+                            
                             </> )) }
                         </div>
                     </div>
