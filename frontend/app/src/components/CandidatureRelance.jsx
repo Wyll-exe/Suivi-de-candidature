5fbe00f260f345147562887f57328adfea51593d
William AGOSSAH <williamagossah@gmail.com>
2025-03-29 17:24:03 +0100

    💄 -remove button re-launched

--- frontend/app/src/components/CandidatureRelance.jsx
+++ frontend/app/src/components/CandidatureRelance.jsx
@@ -35,29 +35,24 @@ function CandidatureRelance({ data }) {
             {error && <p>{error.message}</p>}
             {data && data.length > 0 ? (
                 data.map((post) => (
-                    <div key={post.id} className='flex justify-evenly border-2 border-solid bg-cyan-200 rounded-[20px] w-[1000px] max-w-[1400px] drop-shadow-2xl'>
-                        <div className='flex flex-col justify-center items-center gap-5'>
+                    <div key={post.id} className='flex justify-evenly border-2 border-solid bg-cyan-200 rounded-[20px] w-[1055px] max-w-[1400px] drop-shadow-2xl'>
+                        <div className='flex flex-col justify-center items-center gap-3'>
                             <strong> Society </strong>
                             <p>{post.society}</p>
                         </div>
-                        <div className='flex flex-col justify-center items-center gap-2'>
+                        <div className='flex flex-col justify-center items-center gap-3'>
                             <strong> Job </strong>
                             <p>{post.job}</p>
                         </div>
-                        <div className='flex flex-col justify-center items-center gap-2'>
+                        <div className='flex flex-col justify-center items-center gap-3'>
                             <strong> Not re-launched for </strong>
                             <p>{Number(post.follow)} d </p>
                         </div>
-                        <div className='flex flex-col justify-center items-center gap-2'>
+                        <div className='flex flex-col justify-center items-center gap-3'>
                             <strong>Status</strong>
                             <p>{post.status}</p>
                         </div>
-                        <div className='flex flex-col justify-center items-center gap-2'>
-                            <input type="text" placeholder='  reminder date' className='bg-cyan-200 border-b-black border-5 rounded-4xl w-31'/>
-                        </div>
-                        <div className='flex flex-col justify-center items-center gap-2'>
-                            <button className='submit-2'> SEND </button>
-                        </div>
+                        
                     </div>
                 ))
             ) : (
